qtproject/qt-apps-qtwebbrowser

commit 60a8ef724c11c35413d42ef1ccd118e9c4f6c3c9

Clean up the application after integration for b2qt
* put plugin.cpp into b2qt demo repository
* use resource file
* use AppEngine instead of WebEngine, WebEngine is already
registered with QtWebEgnine
* remove BrowserWindow class
* fix warnings and undefined errors in qml
* rename engine to appengine, qml name collision with launcherengine

Change-Id: I36b682a6a5e3c8f1453c9511c52561d0ff511e71
Reviewed-by: Joerg Bornemann <joerg.bornemann@theqtcompany.com>

src/qml/BrowserWindow.qml
@@ -215,7 +215,9 @@ Item {
                 return
 
             navigation.webView = tab.webView
-            navigation.load(WebEngine.initialUrl);
+            var url = AppEngine.initialUrl
+
+            navigation.load();
         }
         onCurrentIndexChanged: {
             if (!tabView.get(tabView.currentIndex))
@@ -230,7 +232,7 @@ Item {
         property var acceptedUrls : []
 
         function shouldAutoAccept(certificateError){
-            var domain = WebEngine.domainFromString(certificateError.url)
+            var domain = AppEngine.domainFromString(certificateError.url)
             return acceptedUrls.indexOf(domain) >= 0
         }
     }
@@ -251,7 +253,7 @@ Item {
                       "Do you wish to override the security check and continue?"
         onYes: {
             var cert = certErrors.shift()
-            var domain = WebEngine.domainFromString(cert.url)
+            var domain = AppEngine.domainFromString(cert.url)
             acceptedCertificates.acceptedUrls.push(domain)
             cert.ignoreCertificateError()
             presentError()
@@ -398,10 +400,10 @@ Item {
                     onClicked: {
                         var string = urlDropDown.searchString
                         var constructedUrl = ""
-                        if (WebEngine.isUrl(string)) {
-                            constructedUrl = WebEngine.fromUserInput(string)
+                        if (AppEngine.isUrl(string)) {
+                            constructedUrl = AppEngine.fromUserInput(string)
                         } else {
-                            constructedUrl = WebEngine.fromUserInput(googleSearchQuery + string)
+                            constructedUrl = AppEngine.fromUserInput(googleSearchQuery + string)
                         }
                         navigation.webView.url = constructedUrl
                         navigation.webView.forceActiveFocus()

qtwebbrowser.pro
@@ -1,3 +1,2 @@
 TEMPLATE = subdirs
-
 SUBDIRS = src

src/app/app.pro
@@ -1,53 +1,0 @@
-TARGET = qtwebbrowser
-
-DESTDIR = ../
-CONFIG += c++11
-CONFIG -= app_bundle
-
-SOURCES = main.cpp \
-    browserwindow.cpp \
-    engine.cpp
-
-HEADERS = browserwindow.h \
-    engine.h
-
-OTHER_FILES = \
-    qml/assets/UIButton.qml \
-    qml/assets/UIToolBar.qml \
-    qml/ApplicationRoot.qml \
-    qml/BrowserWindow.qml \
-    qml/FeaturePermissionBar.qml \
-    qml/MockTouchPoint.qml \
-    qml/PageView.qml \
-    qml/NavigationBar.qml \
-    qml/HomeScreen.qml \
-    qml/SettingsView.qml \
-
-QT += qml quick webengine
-
-RESOURCES += resources.qrc
-
-!cross_compile {
-    DEFINES += HOST_BUILD
-    SOURCES += touchmockingapplication.cpp \
-            navigationhistoryproxymodel.cpp \
-            touchtracker.cpp
-
-    HEADERS += touchmockingapplication.h \
-                navigationhistoryproxymodel.h \
-                touchtracker.h
-
-    QT_PRIVATE += quick-private gui-private core-private
-}
-else {
-    DESTPATH = /data/user/qt/qtwebbrowser
-
-    content.files = qml/*
-    content.path = $$DESTPATH
-    target.path = $$DESTPATH
-
-    INSTALLS += target content
-}
-
-
-

src/app/browserwindow.cpp
@@ -1,120 +1,0 @@
-/****************************************************************************
-**
-** Copyright (C) 2015 The Qt Company Ltd.
-** Contact: http://www.qt.io/licensing/
-**
-** This file is part of the QtBrowser project.
-**
-** $QT_BEGIN_LICENSE:GPL$
-** Commercial License Usage
-** Licensees holding valid commercial Qt licenses may use this file in
-** accordance with the commercial license agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see http://www.qt.io/terms-conditions. For further
-** information use the contact form at http://www.qt.io/contact-us.
-**
-** GNU General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU
-** General Public License version 2 as published by the Free Software
-** Foundation and appearing in the file LICENSE.GPLv2 included in the
-** packaging of this file. Please review the following information to
-** ensure the GNU General Public License version 2 requirements
-** will be met: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html.
-**
-** GNU General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU
-** General Public License version 3.0 as published by the Free Software
-** Foundation and appearing in the file LICENSE.GPL included in the
-** packaging of this file. Please review the following information to
-** ensure the GNU General Public License version 3.0 requirements will be
-** met: http://www.gnu.org/copyleft/gpl.html.
-**
-**
-** $QT_END_LICENSE$
-**
-****************************************************************************/
-
-#include "browserwindow.h"
-
-#include <QList>
-#include <QQmlContext>
-#include <QQmlEngine>
-#include <QQuickItem>
-#include <QRectF>
-#include <QUrl>
-#include <QVariant>
-
-#include "engine.h"
-
-void BrowserWindow::ensureProfileInstance()
-{
-    if (m_lazyProfileInstance)
-        return;
-    QQmlComponent *component =  new QQmlComponent(engine(), this);
-
-    component->setData(
-                QByteArrayLiteral("import QtQuick 2.0\n"
-                                  "import QtWebEngine 1.1\n"
-                                  "WebEngineProfile {\n"
-                                  "  storageName: \"YABProfile\"\n"
-                                  "}")
-                , QUrl());
-    m_lazyProfileInstance = component->create(engine()->rootContext());
-    Q_ASSERT(m_lazyProfileInstance);
-    QQmlEngine::setObjectOwnership(m_lazyProfileInstance, QQmlEngine::JavaScriptOwnership);
-}
-
-QObject *BrowserWindow::defaultProfile()
-{
-    ensureProfileInstance();
-    return m_lazyProfileInstance;
-}
-
-BrowserWindow::BrowserWindow(QWindow *)
-    : m_lazyProfileInstance(0)
-{
-    setTitle("Yet Another Browser");
-    setFlags(Qt::Window | Qt::WindowTitleHint);
-    setResizeMode(QQuickView::SizeRootObjectToView);
-    setColor(Qt::black);
-
-    engine()->rootContext()->setContextProperty("WebEngine", new Engine(this));
-    setSource(QUrl("qrc:///qml/BrowserWindow.qml"));
-}
-
-BrowserWindow::~BrowserWindow()
-{
-}
-
-void BrowserWindow::updateVisualMockTouchPoints(const QList<QTouchEvent::TouchPoint>& touchPoints)
-{
-    if (touchPoints.isEmpty()) {
-        // Hide all touch indicator items.
-        foreach (QQuickItem* item, m_activeMockComponents.values())
-            item->setProperty("pressed", false);
-
-        return;
-    }
-
-    foreach (const QTouchEvent::TouchPoint& touchPoint, touchPoints) {
-        QQuickItem* mockTouchPointItem = m_activeMockComponents.value(touchPoint.id());
-
-        if (!mockTouchPointItem) {
-            QQmlComponent touchMockPointComponent(engine(), QUrl("qrc:///qml/MockTouchPoint.qml"));
-            mockTouchPointItem = qobject_cast<QQuickItem*>(touchMockPointComponent.create());
-            Q_ASSERT(mockTouchPointItem);
-            m_activeMockComponents.insert(touchPoint.id(), mockTouchPointItem);
-            mockTouchPointItem->setProperty("pointId", QVariant(touchPoint.id()));
-            mockTouchPointItem->setParent(rootObject());
-            mockTouchPointItem->setParentItem(rootObject());
-        }
-
-        QRectF touchRect = touchPoint.rect();
-        mockTouchPointItem->setX(touchRect.center().x());
-        mockTouchPointItem->setY(touchRect.center().y());
-        mockTouchPointItem->setWidth(touchRect.width());
-        mockTouchPointItem->setHeight(touchRect.height());
-        mockTouchPointItem->setProperty("pressed", QVariant(touchPoint.state() != Qt::TouchPointReleased));
-    }
-}

src/app/engine.h
@@ -1,103 +1,0 @@
-/****************************************************************************
-**
-** Copyright (C) 2015 The Qt Company Ltd.
-** Contact: http://www.qt.io/licensing/
-**
-** This file is part of the QtBrowser project.
-**
-** $QT_BEGIN_LICENSE:GPL$
-** Commercial License Usage
-** Licensees holding valid commercial Qt licenses may use this file in
-** accordance with the commercial license agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see http://www.qt.io/terms-conditions. For further
-** information use the contact form at http://www.qt.io/contact-us.
-**
-** GNU General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU
-** General Public License version 2 as published by the Free Software
-** Foundation and appearing in the file LICENSE.GPLv2 included in the
-** packaging of this file. Please review the following information to
-** ensure the GNU General Public License version 2 requirements
-** will be met: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html.
-**
-** GNU General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU
-** General Public License version 3.0 as published by the Free Software
-** Foundation and appearing in the file LICENSE.GPL included in the
-** packaging of this file. Please review the following information to
-** ensure the GNU General Public License version 3.0 requirements will be
-** met: http://www.gnu.org/copyleft/gpl.html.
-**
-**
-** $QT_END_LICENSE$
-**
-****************************************************************************/
-
-#ifndef ENGINE_H
-#define ENGINE_H
-
-#include <QtCore/QEvent>
-#include <QtCore/QFileInfo>
-#include <QtCore/QSettings>
-#include <QtCore/QUrl>
-#include <QtGui/QColor>
-#include <QtQuick/QQuickItemGrabResult>
-
-namespace utils {
-inline bool isTouchEvent(const QEvent* event)
-{
-    switch (event->type()) {
-    case QEvent::TouchBegin:
-    case QEvent::TouchUpdate:
-    case QEvent::TouchEnd:
-        return true;
-    default:
-        return false;
-    }
-}
-
-inline bool isMouseEvent(const QEvent* event)
-{
-    switch (event->type()) {
-    case QEvent::MouseButtonPress:
-    case QEvent::MouseMove:
-    case QEvent::MouseButtonRelease:
-    case QEvent::MouseButtonDblClick:
-        return true;
-    default:
-        return false;
-    }
-}
-
-}
-
-class Engine : public QObject {
-    Q_OBJECT
-
-    Q_PROPERTY(QObject * rootWindow READ rootWindow FINAL CONSTANT)
-    Q_PROPERTY(QString settingsPath READ settingsPath FINAL CONSTANT)
-    Q_PROPERTY(QString initialUrl READ initialUrl FINAL CONSTANT)
-
-    QSettings m_settings;
-    QString m_initialUrl;
-
-public:
-    Engine(QObject *parent = 0);
-    QObject *rootWindow()
-    {
-        return parent();
-    }
-    QString settingsPath();
-    QString initialUrl() const;
-
-    Q_INVOKABLE bool isUrl(const QString& userInput);
-    Q_INVOKABLE QUrl fromUserInput(const QString& userInput);
-    Q_INVOKABLE QString domainFromString(const QString& urlString);
-    Q_INVOKABLE QString fallbackColor();
-    Q_INVOKABLE QString restoreSetting(const QString &name, const QString &defaultValue = QString());
-    Q_INVOKABLE void saveSetting(const QString &name, const QString &value);
-};
-
-#endif // ENGINE_H

src/appengine.cpp
@@ -35,14 +35,14 @@
 **
 ****************************************************************************/
 
-#include "engine.h"
+#include "appengine.h"
 
 #include <QtCore/QDir>
 #include <QtCore/QStandardPaths>
 #include <QStringBuilder>
 #include <QCoreApplication>
 
-Engine::Engine(QObject *parent)
+AppEngine::AppEngine(QObject *parent)
     : QObject(parent)
     , m_settings(QStandardPaths::writableLocation(QStandardPaths::AppDataLocation) % QDir::separator() % "settings.ini", QSettings::IniFormat, this)
 {
@@ -57,17 +57,17 @@ Engine::Engine(QObject *parent)
     }
 }
 
-QString Engine::settingsPath()
+QString AppEngine::settingsPath()
 {
     return m_settings.fileName();
 }
 
-QString Engine::initialUrl() const
+QString AppEngine::initialUrl() const
 {
     return m_initialUrl;
 }
 
-QUrl Engine::fromUserInput(const QString& userInput)
+QUrl AppEngine::fromUserInput(const QString& userInput)
 {
     QFileInfo fileInfo(userInput);
     if (fileInfo.exists())
@@ -75,7 +75,7 @@ QUrl Engine::fromUserInput(const QString& userInput)
     return QUrl::fromUserInput(userInput);
 }
 
-bool Engine::isUrl(const QString& userInput)
+bool AppEngine::isUrl(const QString& userInput)
 {
     if (userInput.startsWith(QStringLiteral("www."))
             || userInput.startsWith(QStringLiteral("http"))
@@ -86,12 +86,12 @@ bool Engine::isUrl(const QString& userInput)
     return false;
 }
 
-QString Engine::domainFromString(const QString& urlString)
+QString AppEngine::domainFromString(const QString& urlString)
 {
     return QUrl::fromUserInput(urlString).host();
 }
 
-QString Engine::fallbackColor()
+QString AppEngine::fallbackColor()
 {
     static QList<QString> colors = QList<QString>() << QStringLiteral("#46a2da")
                                                     << QStringLiteral("#18394c")
@@ -103,12 +103,13 @@ QString Engine::fallbackColor()
     return colors[index];
 }
 
-QString Engine::restoreSetting(const QString &name, const QString &defaultValue)
+QString AppEngine::restoreSetting(const QString &name, const QString &defaultValue)
 {
     return m_settings.value(name, defaultValue).toString();
 }
 
-void Engine::saveSetting(const QString &name, const QString &value)
+void AppEngine::saveSetting(const QString &name, const QString &value)
 {
     m_settings.setValue(name, value);
 }
+

src/appengine.h
@@ -35,30 +35,66 @@
 **
 ****************************************************************************/
 
-#ifndef BROWSERWINDOW_H
-#define BROWSERWINDOW_H
+#ifndef APPENGINE_H
+#define APPENGINE_H
 
-#include <QHash>
-#include <QTouchEvent>
-#include <QQuickView>
-#include <QQmlComponent>
+#include <QtCore/QEvent>
+#include <QtCore/QFileInfo>
+#include <QtCore/QSettings>
+#include <QtCore/QUrl>
+#include <QtGui/QColor>
+#include <QtQuick/QQuickItemGrabResult>
 
-class BrowserWindow : public QQuickView
+namespace utils {
+inline bool isTouchEvent(const QEvent* event)
 {
+    switch (event->type()) {
+    case QEvent::TouchBegin:
+    case QEvent::TouchUpdate:
+    case QEvent::TouchEnd:
+        return true;
+    default:
+        return false;
+    }
+}
+
+inline bool isMouseEvent(const QEvent* event)
+{
+    switch (event->type()) {
+    case QEvent::MouseButtonPress:
+    case QEvent::MouseMove:
+    case QEvent::MouseButtonRelease:
+    case QEvent::MouseButtonDblClick:
+        return true;
+    default:
+        return false;
+    }
+}
+
+}
+
+class AppEngine : public QObject {
     Q_OBJECT
 
-    QHash<int, QQuickItem*> m_activeMockComponents;
-    QObject *m_lazyProfileInstance;
+    Q_PROPERTY(QString settingsPath READ settingsPath FINAL CONSTANT)
+    Q_PROPERTY(QString initialUrl READ initialUrl FINAL CONSTANT)
 
-    void ensureProfileInstance();
 public:
-    BrowserWindow(QWindow *parent = 0);
-    ~BrowserWindow();
+    AppEngine(QObject *parent = 0);
 
-    void updateVisualMockTouchPoints(const QList<QTouchEvent::TouchPoint>& touchPoints);
+    QString settingsPath();
+    QString initialUrl() const;
 
-public Q_SLOTS:
-    QObject *defaultProfile();
+    Q_INVOKABLE bool isUrl(const QString& userInput);
+    Q_INVOKABLE QUrl fromUserInput(const QString& userInput);
+    Q_INVOKABLE QString domainFromString(const QString& urlString);
+    Q_INVOKABLE QString fallbackColor();
+    Q_INVOKABLE QString restoreSetting(const QString &name, const QString &defaultValue = QString());
+    Q_INVOKABLE void saveSetting(const QString &name, const QString &value);
+
+private:
+    QSettings m_settings;
+    QString m_initialUrl;
 };
 
-#endif // BROWSERWINDOW_H
+#endif // APPENGINE_H

src/imports/imports.pro
@@ -1,17 +1,0 @@
-CXX_MODULE = qml
-TARGET = webbrowser
-TARGETPATH = WebBrowser
-QT += qml quick
-CONFIG += qt
-
-SOURCES += \
-    plugin.cpp
-
-load(qml_plugin)
-
-target.path += /data/user/qt/qmlplugins/WebBrowser
-INSTALLS += target
-
-
-
-

commit 7036e53c5f40591c27d69d1eb315f6ef3b041244

qtwebbrowser: load initialUrl
fix regression of setting a initial url via argv.
Fixes: 60a8ef724c11 ("Clean up the application after integration for
b2qt")

Signed-off-by: Jan Remmet <j.remmet@phytec.de>

src/qml/BrowserWindow.qml
@@ -207,7 +207,7 @@ Item {
             navigation.webView = tab.webView
             var url = AppEngine.initialUrl
 
-            navigation.load();
+            navigation.load(url);
         }
         onCurrentIndexChanged: {
             if (!tabView.get(tabView.currentIndex))